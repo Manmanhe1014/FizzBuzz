1f83f34277ec88461dc55851d2bad5f2b27518ed
Amanda He <amandahe@Amandas-MacBook-Air-4.local>
2024-09-12 00:04:24 -0400

    refactored to extract helper method

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,11 +1,21 @@
 public class Multiples {
-    public static void main(String[] args) {
+    public static int main(int n, int a, int b) {
+        int c = 0;
+        for(int i = 1; i <= n; i++) {
+            if (i % a ==0 || i % b ==0) {
+                c++;
+            }
+        }
+        return c;
+    }
+
+    public static int main(String[] args) {
         int c = 0;
         for(int i = 1; i <= 1000; i++) {
             if (i % 3 ==0 || i % 5 ==0) {
                 c++;
             }
         }
-        System.out.println(c);
+        return c;
     }
 }
